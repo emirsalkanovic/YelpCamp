13b4619fc2c06851afdbd0a796cc760216e132d7
Emir <emirsalkanovic1@gmail.com>
2018-05-27 08:49:23 +0000

    update dburl

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://emir:alma@ds137650.mlab.com:37650/yelpcamp");
+ mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("mongodb://emir:alma@ds137650.mlab.com:37650/yelpcamp");
 // mongodb://emir:alma@ds137650.mlab.com:37650/yelpcamp
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
